7119befad8d37caeb3eca340f1016b0c3907240c
Ekaterina Yurkova <kniffligHexe@yandex.ru>
2023-05-29 15:06:25 +0800

    "change_getDataHeaders_in_fronend"

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,6 @@
 const getDataHeaders = {
   //authorization: "8db76d83-4308-4ed0-aecc-97a862cc1cdd",
+  "Content-Type": "application/json",
 };
 const setDataHeaders = {
   //authorization: "8db76d83-4308-4ed0-aecc-97a862cc1cdd",
